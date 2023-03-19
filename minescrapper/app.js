c4aa1e5097262e3dcf618dab2457971cc6936e6b
sklbz <sokolbozanic@gmail.com>
2023-03-19 11:16:18 +0100

    i got the number of bomb around each el, starting to display it

--- minescrapper/app.js
+++ minescrapper/app.js
@@ -14,7 +14,11 @@ let cases = [
 let bombsCount = 0;
 
 const reveal = el => {
-    el.classList.replace("masked", "revealed");
+  el.classList.replace("masked", "revealed");
+	el.removeEventListener('click', () => {
+		console.log('clicked');
+		reveal(el);
+	});
 }
 
 const init = () => {
@@ -22,6 +26,7 @@ const init = () => {
 	for(let i = 0; i < 100; i++){
 		const div = document.createElement("div");
 		div.classList.add('masked')
+		div.dataset.index = i;
 		const y = (Math.floor(i / 10) > i / 10) ? Math.floor(i/10) - 1 : Math.floor(i/10);
 		const x = i % 10;
 		if (Math.floor(Math.random()*10) === 1) {
@@ -33,7 +38,10 @@ const init = () => {
 				init();
 			});
 		} else{
-			div.addEventListener('click', div => reveal(div));
+			div.addEventListener('click', () => {
+				console.log('clicked');
+				reveal(div);
+			});
 			div.dataset.x = x;
 			div.dataset.y = y;
 		} 
@@ -50,17 +58,17 @@ notBombs.forEach(e => {
 	const y =  Number(e.dataset.y);
 	const x = Number(e.dataset.x);
 	let bombAround = 0;
-	let t = 0;
+	console.log(`Looking around cases[${y}][${x}]`)
 	for(let i = -1; i<=1;i++){
 		for(let j = -1; j<=1;j++){
-			if (y+i <= 10 && x+j <=10 && y+i > -1 && x+j >-1){
-				console.log(cases[y+i][x+j]); t++;
-				if (cases[y+i][x+j] === -1) bombAround++;
+			if (cases[y+i] != undefined){
+				if (cases[y+i][x+j] != undefined){
+					console.log(`Current testing cases[${y+i}][${x+j}]`);
+					if (cases[y+i][x+j] === -1) bombAround++;
+			}
 			}
 		}
 	}
-	console.log("t " + t);
-	a++
-	console.log("a", a)
-	e.innerHTML = `<p>${bombAround}</p>`
+	e.dataset.bombAround = bombAround;
+	e.innerText = `${bombAround}`
 })
